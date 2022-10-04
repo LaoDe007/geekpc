70aac08990cc40c80828435fc81679eb03060c48
laode <depeng0128@163.com>
2022-10-04 15:16:43 +0800

    表单结构-表单验证

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -19,41 +19,69 @@ export default function Login({}: Props) {
       <Card className="login-container">
         <div className="login-logo">登录</div>
         <Form
-          name="basic"
           labelCol={{ span: 5 }}
           wrapperCol={{ span: 19 }}
-          initialValues={{ remember: true }}
+          labelAlign="left"
+          size="large"
+          autoComplete="off"
           onFinish={onFinish}
           onFinishFailed={onFinishFailed}
-          autoComplete="off"
-          labelAlign="left"
+          initialValues={{ mobile: 13911111111, code: 123456, agree: false }}
         >
           <Form.Item
             label="用户名"
-            name="username"
-            rules={[{ required: true, message: "Please input your username!" }]}
+            name="mobile"
+            validateTrigger={["onBlur", "onChange"]}
+            rules={[
+              { required: true, message: "手机号不能为空" },
+              {
+                pattern: /^1[3-9]\d{9}$/,
+                message: "手机号格式错误",
+              },
+            ]}
           >
-            <Input />
+            <Input placeholder="请输入您的手机号" />
           </Form.Item>
 
           <Form.Item
-            label="密码"
-            name="password"
-            rules={[{ required: true, message: "Please input your password!" }]}
+            label="验证码"
+            name="code"
+            rules={[
+              { required: true, message: "验证码不能为空" },
+              {
+                pattern: /^\d{6}$/,
+                message: "验证码格式错误",
+              },
+            ]}
           >
-            <Input.Password />
+            <Input placeholder="请输入您的验证码" />
           </Form.Item>
 
           <Form.Item
-            name="remember"
+            name="agree"
             valuePropName="checked"
-            wrapperCol={{ offset: 8, span: 16 }}
+            wrapperCol={{ offset: 5, span: 19 }}
+            rules={[
+              {
+                //自定义校验规则
+                validator(_, value) {
+                  if (value) {
+                    return Promise.resolve();
+                  }
+                  return Promise.reject(new Error("请阅读并阅读用户协议"));
+                },
+              },
+              // {
+              //   pattern: /^true$/,
+              //   message: "请阅读[隐私条款]和[用户协议]",
+              // },
+            ]}
           >
-            <Checkbox>记住账号密码</Checkbox>
+            <Checkbox>我已阅读并同意[隐私条款]和[用户协议]</Checkbox>
           </Form.Item>
 
-          <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-            <Button type="primary" htmlType="submit">
+          <Form.Item wrapperCol={{ offset: 5, span: 19 }}>
+            <Button type="primary" htmlType="submit" block>
               登录
             </Button>
           </Form.Item>
